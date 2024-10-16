2b36801f1b5feeca66322d0b15a4de78bb9aa97a
Mehmet Fesli <mehmetamerica@gmail.com>
2024-10-16 16:03:22 -0400

    Update API key

--- index.js
+++ index.js
@@ -5,18 +5,16 @@ const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
 
 search.addEventListener('click', () => {
-
-    const APIKey = '6dbd61a7245124f0b4e9eb5b3c03dcc1';
+    const APIKey = '60ae41103acc4e14812195349241610';
     const city = document.querySelector('.search-box input').value;
 
     if (city === '')
         return;
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+    fetch(`https://api.weatherapi.com/v1/current.json?key=${APIKey}&q=${city}`)
         .then(response => response.json())
         .then(json => {
-
-            if (json.cod === '404') {
+            if (json.error && json.error.code === 1006) {
                 container.style.height = '400px';
                 weatherBox.style.display = 'none';
                 weatherDetails.style.display = 'none';
@@ -34,44 +32,44 @@ search.addEventListener('click', () => {
             const humidity = document.querySelector('.weather-details .humidity span');
             const wind = document.querySelector('.weather-details .wind span');
 
-            switch (json.weather[0].main) {
-                case 'Clear':
+            switch (json.current.condition.text.toLowerCase()) {
+                case 'sunny':
+                case 'clear':
                     image.src = 'images/clear.png';
                     break;
-
-                case 'Rain':
+                case 'rain':
+                case 'light rain':
+                case 'moderate rain':
                     image.src = 'images/rain.png';
                     break;
-
-                case 'Snow':
+                case 'snow':
+                case 'light snow':
+                case 'moderate snow':
                     image.src = 'images/snow.png';
                     break;
-
-                case 'Clouds':
+                case 'cloudy':
+                case 'partly cloudy':
+                case 'overcast':
                     image.src = 'images/cloud.png';
                     break;
-
-                case 'Haze':
+                case 'mist':
+                case 'fog':
+                case 'haze':
                     image.src = 'images/mist.png';
                     break;
-
                 default:
                     image.src = '';
             }
 
-            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
-            description.innerHTML = `${json.weather[0].description}`;
-            humidity.innerHTML = `${json.main.humidity}%`;
-            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+            temperature.innerHTML = `${json.current.temp_f.toFixed(1)}<span>°F</span>`;
+            description.innerHTML = json.current.condition.text;
+            humidity.innerHTML = `${json.current.humidity}%`;
+            wind.innerHTML = `${json.current.wind_mph.toFixed(1)}Km/h`;
 
             weatherBox.style.display = '';
             weatherDetails.style.display = '';
             weatherBox.classList.add('fadeIn');
             weatherDetails.classList.add('fadeIn');
             container.style.height = '590px';
-
-
         });
-
-
 });
